bca2a402d39b1f84f3f8de0e2f2e0a523e3df645
Jeremy Permaul <jnpermaul@gmail.com>
2024-06-11 20:05:20 -0700

    I hope this works

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
+    outDir: 'dist',
   },
 });
